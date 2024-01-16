57d2e611349ffdd66c244500b395d13e9e134a42
Jon Shaulis <jon@mscreations.net>
2024-01-16 00:56:07 -0500

    Add check to verify whether updating hash is necessary
    Fixes issue where build would occur every time because version-git.h was changed every build. If current hash is same as in file, skips updating file.
    
    Update custom build command

--- updateversion.ps1
+++ updateversion.ps1
@@ -1,3 +1,7 @@
 $gitver = $(git rev-parse --short HEAD)
 $file = "./include/version-git.h"
-(Get-Content $file) -replace "^#define VERSION_BUILD\w*.*$", "#define VERSION_BUILD       $gitver" | Set-Content $file
+if ((Select-String -Path $file -Pattern $gitver).Matches.Length -ne 1)
+{
+    (Get-Content $file) -replace "^#define VERSION_BUILD\w*.*$", "#define VERSION_BUILD       $gitver" | Set-Content $file
+}
+
